b722e3ab53708d3a85aa156de9c34665463c8e74
wjtch <91051322+wjtch@users.noreply.github.com>
2021-10-13 07:44:20 +0700

    Dien tich hinh tron
    Cong thuc dien tich hinh tron: π*pow(r,2).
    Phan hinh ve trong anh duoc chia lam 4 nen cong thuc se la 1/2π*pow(r,2).

---
+++ Dien tich hinh tron anh.cpp
@@ -0,0 +1,15 @@
+//tinh cong thuc dien tich hinh tron
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	const float pi = 3.14;
+	float R;
+	cout << "Nhap ban kinh duong tron: ";
+	cin>>R;
+	
+	float S = 0.5 * pi * pow(R,2);
+	cout<<"Dien tich hinh tron la: "<< S;
+	return 0;
+}
